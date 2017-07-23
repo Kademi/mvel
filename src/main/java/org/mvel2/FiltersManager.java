1787e5680ac298dd6ba51e3b368969e3ace5b27b
MohammedFuad97 <mhmdowda@gmail.com>
2017-07-23 11:48:27 +0300

    Added description for FiltersManager interface.

--- src/main/java/org/mvel2/FiltersManager.java
+++ src/main/java/org/mvel2/FiltersManager.java
@@ -18,7 +18,25 @@ package org.mvel2;
 import jdk.nashorn.api.scripting.ClassFilter;
 
 /**
+ * This interface works on providing MVEL with the ability of creating
+ * filter for classes which are going to be invoked from the HTML and execute
+ * during the process of rendering MVEL template.
  *
+ * All what you need to start using this interface and filtering MVEL is to
+ * add a property to ParserContext once before evaluating any template or 
+ * expression. So MVEL is going to filter the ability of reaching any of 
+ * the classes depends on what are you going to implement at
+ * (class FiltersManagerClass implements
+ * FiltersManager) in your project.
+ *
+ * NOTE: anytime exposeToScripts() returns false, that means velocity is going
+ * to reject the access to this class through HTML developers.
+ *
+ *
+ * e.g.: ParserContext ctx = new ParserContext();
+ *       ctx.addProperty("mvel.filtersManager", "com.mvel.test.FiltersManager");
+ *       MVEL.evalToString("....", ....);
+ * 
  * @author Mohammed F. Ouda
  */
 public interface FiltersManager {
